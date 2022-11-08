f5b4c375adfbe6e3b97006f2c345159ae4f077cd
Celal AKSU <celalaksu@gmail.com>
2022-11-09 02:20:03 +0300

    changes for nft

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,36 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect, useState } from "react";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { extendTheme } from "@chakra-ui/react";
+
+import WalletContextProvider from "../components/WalletContextProvider";
+
+const colors = {
+  background: "#1F1F1F",
+  accent: "#833BBE",
+  bodyText: "rgba(255, 255, 255, 0.75)",
+};
+
+const theme = extendTheme({ colors });
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const [showChild, setShowChild] = useState(false);
+
+  useEffect(() => {
+    setShowChild(true);
+  }, []);
+
+  if (!showChild) {
+    return null;
+  }
+  return (
+    <ChakraProvider theme={theme}>
+      <WalletContextProvider>
+        <Component {...pageProps} />
+      </WalletContextProvider>
+    </ChakraProvider>
+  );
 }
+
+export default MyApp;
